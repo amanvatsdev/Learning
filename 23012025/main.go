da323c4d00e47ebef8844b58d7d9cb0850a31eaf
Aman vats <freeguystudent@gmail.com>
2025-01-24 01:40:31 +0530

    commit to add version 1.1.1 version

--- 23012025/main.go
+++ 23012025/main.go
@@ -8,18 +8,23 @@ import (
 	"github.com/amanvatsdev/Learning/23012025/dependecy/Sound"
 )
 
-func main() {
-	AdultDog:=Bark.AuldtDog()
-	Puupy:=Bark.Puppy()
-	CatSound:=Sound.Cat()
-	HumanSound:=Sound.Human()
-	AnimalSounds:=[]string{AdultDog,Puupy,CatSound,HumanSound}
-	
-	for SeraialNumber,Value:=range AnimalSounds{
-	
-		fmt.Println(SeraialNumber+1,Value)
+func Tag14() {
+	fmt.Println("This is for tag 1.1.1")
+}
 
-	//using of tag version package here
-	TagVersion.PracticingVersion()
+func main() {
+	AdultDog := Bark.AuldtDog()
+	Puupy := Bark.Puppy()
+	CatSound := Sound.Cat()
+	HumanSound := Sound.Human()
+	AnimalSounds := []string{AdultDog, Puupy, CatSound, HumanSound}
+
+	for SeraialNumber, Value := range AnimalSounds {
+
+		fmt.Println(SeraialNumber+1, Value)
+
+		//using of tag version package here
+		TagVersion.PracticingVersion()
+		Tag14()
 	}
 }
